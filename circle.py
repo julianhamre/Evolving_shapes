a631eaa913beeb6261d69a71c0f448de5640ab15
Julian Hamre <julian.hamre5@gmail.com>
2022-07-04 18:05:54 +0200

    add get_points method

--- circle.py
+++ circle.py
@@ -7,14 +7,15 @@ Created on Sun Jul  3 20:30:20 2022
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 class circle:
     speed = 2
     
-    def __init__(self, radius, x, y):
+    def __init__(self, radius, x_coordinate, y_coordinate):
         self.radius = radius
-        self.x = x
-        self.y = y
+        self.x = x_coordinate
+        self.y = y_coordinate
     
     def move(self, direction_angle, time):
         distance = self.speed * time
@@ -22,4 +23,11 @@ class circle:
         self.x += np.cos(radians_angle) * distance
         self.y += np.sin(radians_angle) * distance
 
-
+    def get_points(self):
+        angles = np.linspace(0, 2*np.pi, 100)
+        x = []
+        y = []
+        for angle in angles:
+            x.append(self.radius * np.cos(angle) + self.x)
+            y.append(self.radius * np.sin(angle) + self.y)
+        return [x, y]
