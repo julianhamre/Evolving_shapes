76d85f5ad2eb76b68a301a6f2ea5f1230f773f92
Julian Hamre <julian.hamre5@gmail.com>
2022-07-04 15:21:55 +0200

    add direction_angle parameter in move method

--- circle.py
+++ circle.py
@@ -6,6 +6,8 @@ Created on Sun Jul  3 20:30:20 2022
 @author: julianhamre
 """
 
+import numpy as np
+
 class circle:
     speed = 2
     
@@ -14,7 +16,10 @@ class circle:
         self.x = x
         self.y = y
     
-    def move(self, time):
-        self.x += self.speed * time
+    def move(self, direction_angle, time):
+        distance = self.speed * time
+        radians_angle = np.radians(direction_angle)
+        self.x += np.cos(radians_angle) * distance
+        self.y += np.sin(radians_angle) * distance
 
 
--- test_circle.py
+++ test_circle.py
@@ -12,11 +12,20 @@ import circle as cl
 class test_circle(unittest.TestCase):
     
     def setUp(self):
-        self.c = cl.circle(1, 2, 2)
-    
+        self.c = cl.circle(1, 3.6, 5)
+
     def test_move(self):
-        self.c.move(3)
-        self.assertEqual(self.c.x, 8)
+        easy_c = cl.circle(1, 0, 0)
+        easy_c.move(45, 1)
+        self.assertAlmostEqual(easy_c.x, 2**(1/2), 2)
+        self.assertAlmostEqual(easy_c.y, 2**(1/2), 2)
+
+"""    
+    def test_move(self):
+        self.c.move(61.2, 11)
+        self.assertAlmostEqual(self.c.x, 14.1986, 2)
+        self.assertAlmostEqual(self.c.y, 24.2787, 2)
+"""
 
 if __name__ == "__main__":
     unittest.main()
